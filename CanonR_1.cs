02a65c19b8bd6abcb89b7b493cea2871dd0447e8
kuarisu <kuarisuran@gmail.com>
2015-12-12 10:39:06 +0100

    testeuh

---
+++ CanonR_1.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CanonR_1 : MonoBehaviour
+{
+
+    //void OnTriggerEnter(Collider col)
+    //{
+    //    if (col.gameObject.tag == "Obs_small")
+    //    {
+    //        col.gameObject.GetComponent<Rigidbody>().isKinematic = false;
+    //        col.gameObject.GetComponent<Rigidbody>().AddForce(transform.forward * 3);
+
+    //    }
+    //    if (col.gameObject.tag == "Obs_medium")
+    //    {
+    //        col.gameObject.GetComponent<Rigidbody>().isKinematic = false;
+    //        col.gameObject.GetComponent<Rigidbody>().AddForce(transform.forward * 2);
+    //    }
+    //    if (col.gameObject.tag == "Obs_big")
+    //    {
+    //        col.gameObject.GetComponent<Rigidbody>().isKinematic = false;
+    //        col.gameObject.GetComponent<Rigidbody>().AddForce(transform.forward * 1);
+    //    }
+    //}
+}
